ded10503891be2352bb094a8692b783685416aa5
Fno1996 <franciele_oliveira123@hotmail.com>
2025-04-13 14:22:10 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 async def root():
     return{"message": "Hello Word"}
 
-# 127.0.0.1:/8000/teste
+# 127.0.0.1:/8000/teste1
 @app.get("/teste1")
 async def funcaoteste():
     return {"teste" : "deu certo"}
